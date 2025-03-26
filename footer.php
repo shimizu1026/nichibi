9893a923d5966c4375fe3f8c720e98a0f2319afb
nichibiWin01 <yamamoto@nichibi.co.jp>
2025-03-26 09:48:25 +0900

    0326yamamoto

--- footer.php
+++ footer.php
@@ -1,59 +1,63 @@
 <div class="pf-banner">
-	<a href="peacefactory.html">
-		<picture>
-			<source srcset="img/peacefactory/pf-banner.jpg" media="(min-width: 640px)">
-			<img src="img/peacefactory/pf-banner-sp.jpg" alt="PeaceFactory 社会貢献を目的とした新しい事業です" width="1200" height="160" decoding="async" loading="lazy"></picture></a></div>
+ <a href="peacefactory.html">
+  <picture>
+   <source srcset="img/peacefactory/pf-banner.jpg" media="(min-width: 640px)">
+   <img src="img/peacefactory/pf-banner-sp.jpg" alt="PeaceFactory 社会貢献を目的とした新しい事業です" width="1200" height="160" decoding="async" loading="lazy">
+  </picture>
+ </a>
+</div>
 <div class="f-contact flexbox flex-align-center flex-justify-end anniv-contact">
-    <div class="contact-txtbox">
-        <h2 class="heading01">
-            CONTACT
-            <span>お気軽にお問い合わせください</span>
-        </h2>
-    </div><!-- contact-txtbox -->
-    <a href="<?php echo esc_url( home_url( '/' ) ); ?>
-contact"><i class="fa fa-envelope" aria-hidden="true"></i> メールでのお問い合わせ・資料請求</a>
+ <div class="contact-txtbox">
+  <h2 class="heading01">
+   CONTACT
+   <span>お気軽にお問い合わせください</span>
+  </h2>
+ </div><!-- contact-txtbox -->
+ <a href="<?php echo esc_url(home_url('/')); ?>
+contact"><i class="fa fa-envelope" aria-hidden="true"></i> メールでのお問い合わせ</a>
 </div>
 <footer class="footer">
-  <div class="container flexbox flex-justify-between">
-        <div class="adress">
-        <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/common/logo01.png">
-          </a>
-          株式会社 日美
-        </h1>
-            <p>
-                【本社】</br>〒730-0842　広島県広島市中区舟入中町7番1号</br>TEL：<a href="tel:0825322200">082-532-2200</a>　FAX：082-232-7733
-                </br></br>【東京営業所】</br>〒111-0053　東京都台東区浅草橋3-1-8アダックスビル4F</br>TEL：<a href="tel:0356878911">03-5687-8911</a></br></br>
-                <ul class="footer_icons">
-                    <li class="instagram"><a href="https://www.instagram.com/nichibi.official/?hl=ja" target="_blank" classinstagram><i class="fa fa-instagram" aria-hidden="true"></i>Instagram</a></li>
-                    <li class="peacefactory"><a href="https://www.peace-factory.net/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/common/peacefactory_logo.svg" alt=""></a></li>
-                </ul>
-            </p>
-        </div>
-      <div class="f-nav flexbox">
-          <ul>
-              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>concept/">コンセプト</a></li>
-              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>graphic/">グラフィック</a></li>
-              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>it/">ITソリューション</a></li>
-              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>about/">日美について</a></li>
-              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>company/">会社概要</a></li>
-          </ul>
-          <ul>
-              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>performance/">制作実績</a></li>
-              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>news">ニュース</a></li>
-              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>category/blog/">ブログ</a></li>
-          </ul>
-          <ul>
-              <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>contact/">お問い合わせ</a></li>
-          </ul>
-      </div>
+ <div class="container flexbox flex-justify-between">
+  <div class="adress">
+   <h1><a href="<?php echo esc_url(home_url('/')); ?>">
+     <img src="<?php echo esc_url(get_template_directory_uri()); ?>/img/common/logo01.png">
+    </a>
+    株式会社 日美
+   </h1>
+   <p>
+    【本社】</br>〒730-0842　広島県広島市中区舟入中町7番1号</br>TEL：<a href="tel:0825322200">082-532-2200</a>　FAX：082-232-7733
+    </br></br>【東京営業所】</br>〒111-0053　東京都台東区浅草橋3-1-8アダックスビル4F</br>TEL：<a href="tel:0356878911">03-5687-8911</a></br></br>
+   <ul class="footer_icons">
+    <li class="instagram"><a href="https://www.instagram.com/nichibi.official/?hl=ja" target="_blank" classinstagram><i class="fa fa-instagram" aria-hidden="true"></i>Instagram</a></li>
+    <li class="peacefactory"><a href="https://www.peace-factory.net/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/common/peacefactory_logo.svg" alt=""></a></li>
+   </ul>
+   </p>
   </div>
-  <small>Copyright © 2021 NICHIBI All Rights Reserved. </small>
+  <div class="f-nav flexbox">
+   <ul>
+    <li><a href="<?php echo esc_url(home_url('/')); ?>concept/">コンセプト</a></li>
+    <li><a href="<?php echo esc_url(home_url('/')); ?>graphic/">グラフィック</a></li>
+    <li><a href="<?php echo esc_url(home_url('/')); ?>it/">ITソリューション</a></li>
+    <li><a href="<?php echo esc_url(home_url('/')); ?>about/">日美について</a></li>
+    <li><a href="<?php echo esc_url(home_url('/')); ?>company/">会社概要</a></li>
+   </ul>
+   <ul>
+    <li><a href="<?php echo esc_url(home_url('/')); ?>performance/">制作実績</a></li>
+    <li><a href="<?php echo esc_url(home_url('/')); ?>news">ニュース</a></li>
+    <li><a href="<?php echo esc_url(home_url('/')); ?>category/blog/">ブログ</a></li>
+   </ul>
+   <ul>
+    <li><a href="<?php echo esc_url(home_url('/')); ?>contact/">お問い合わせ</a></li>
+   </ul>
+  </div>
+ </div>
+ <small>Copyright © 2021 NICHIBI All Rights Reserved. </small>
 </footer>
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/libs/jquery.min.js"></script>
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/libs/bootstrap.min.js"></script>
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/libs/slick.min.js"></script>
-<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/app.js"></script>
+<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/libs/jquery.min.js"></script>
+<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/libs/bootstrap.min.js"></script>
+<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/libs/slick.min.js"></script>
+<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/app.js"></script>
 <?php wp_footer(); ?>
 </body>
+
 </html>
